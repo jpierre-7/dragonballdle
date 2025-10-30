b52e3d93b3c8ce8089142263cd6523a660c6cf4d
Gabriel Morales <gabbomorales1@gmail.com>
2025-10-30 17:53:20 -0500

    removed upper level directory

--- dragonball-app/src/pages/ability.tsx
+++
@@ -1,7 +0,0 @@
-export default function Ability() {
-  return (
-    <div>
-        <h1>Ability WEINER</h1>
-    </div>
-  );
-}
--- dragonball-app/src/pages/silouhette.tsx
+++
@@ -1,7 +0,0 @@
-export default function Silouhette() {
-  return (
-    <div>
-        <h1>Silouhette WEINER</h1>
-    </div>
-  );
-}
