48710fcb9fca38d26acadcd1acfe7ff50849d90f
Gabriel Morales <gabbomorales1@gmail.com>
2025-10-30 19:10:31 -0500

    added background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   console.log("hello");
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-[url(/img/goku.jpg)] bg-cover bg-center">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16  dark:bg-black opacity-50 sm:items-start">
+        
         <Image
           className="dark:invert"
           src="/next.svg"
